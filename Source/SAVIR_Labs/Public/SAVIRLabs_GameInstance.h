36dff95a82089184dcbea66b9ee5c2c561590816
IslamRabie <eslamrbie@gmail.com>
2021-08-11 19:56:27 +0200

    Add GameInstance Class

---
+++ Source/SAVIR_Labs/Public/SAVIRLabs_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SAVIRLabs_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAVIR_LABS_API USAVIRLabs_GameInstance : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
