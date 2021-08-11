681727e4ee475f5017786d54f1de4b03d42a8c81
IslamRabie <eslamrbie@gmail.com>
2021-08-11 19:29:45 +0200

    Add InstructionsTriggerBox

---
+++ Source/SAVIR_Labs/Public/Components/InstructionTriggerBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "InstructionTriggerBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAVIR_LABS_API AInstructionTriggerBox : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+};
