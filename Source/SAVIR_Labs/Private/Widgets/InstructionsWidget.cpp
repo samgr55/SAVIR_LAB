8814d5943d01b8b3091692e6627d0bd5886c3ee8
IslamRabie <eslamrbie@gmail.com>
2021-08-12 08:48:23 +0200

    update InstructionsWidget

--- Source/SAVIR_Labs/Private/Widgets/InstructionsWidget.cpp
+++ Source/SAVIR_Labs/Private/Widgets/InstructionsWidget.cpp
@@ -3,3 +3,38 @@
 
 #include "Widgets/InstructionsWidget.h"
 
+void UInstructionsWidget::StartNarrative(FString InTitle, TArray<FString>* InNarrative,
+	TArray<float>* InNarrativeTimeInMSecond)
+{
+	Title = InTitle;
+	Narrative = InNarrative;
+	NarrativeTimeInMSecond = InNarrativeTimeInMSecond;
+	TitleText->SetText(FText::FromString(Title));
+	NarrativeViewer();
+}
+
+void UInstructionsWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	if(!TitleText || !NarrativeText)
+	{
+		UE_LOG(LogTemp, Error, TEXT("TitleText or NarrativeText wasnt created in Designer in UInstructionsWidget::NativeOnInitialized"));
+		return;
+	}
+	TitleText->SetText(FText::FromString(""));
+	NarrativeText->SetText((FText::FromString("")));
+}
+
+void UInstructionsWidget::NarrativeViewer()
+{
+	if(Index >= Narrative->Num())
+	{
+		TitleText->SetText(FText::FromString(""));
+		NarrativeText->SetText((FText::FromString("")));
+		return;
+	}
+	NarrativeText->SetText(FText::FromString(Narrative->GetData()[Index]));
+	GetOwningPlayer()->GetWorldTimerManager().SetTimer(NarrativeTimerHandle, this,
+		&UInstructionsWidget::NarrativeViewer, NarrativeTimeInMSecond->GetData()[Index]/1000.f);
+	Index++;
+}
--- Source/SAVIR_Labs/Public/Widgets/InstructionsWidget.h
+++ Source/SAVIR_Labs/Public/Widgets/InstructionsWidget.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "InstructionsWidget.generated.h"
 
 /**
@@ -13,5 +14,33 @@ UCLASS()
 class SAVIR_LABS_API UInstructionsWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+
+public:
+	void StartNarrative(FString InTitle, TArray<FString>* InNarrative, TArray<float>* InNarrativeTimeInMSecond);
+	
+protected:
+	virtual void NativeOnInitialized() override;
+	
+private:
+
+	void NarrativeViewer();
+	
+	FString Title;
+
+	int32 Index = 0;
+
+	TArray<FString>* Narrative;
+
+	TArray<float>* NarrativeTimeInMSecond;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TitleText;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* NarrativeText;
+
+	FTimerHandle NarrativeTimerHandle;
+	
 	
 };
