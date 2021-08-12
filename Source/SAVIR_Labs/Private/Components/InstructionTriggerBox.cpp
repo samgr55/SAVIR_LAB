79e051673329b145eea2e7024f179013261d9957
IslamRabie <eslamrbie@gmail.com>
2021-08-12 08:51:49 +0200

    Update InstructionsTriggerBox

--- Source/SAVIR_Labs/Private/Components/InstructionTriggerBox.cpp
+++ Source/SAVIR_Labs/Private/Components/InstructionTriggerBox.cpp
@@ -3,3 +3,48 @@
 
 #include "Components/InstructionTriggerBox.h"
 
+#include "Components/LookAt.h"
+#include "Widgets/InstructionsHUD.h"
+#include "Widgets/InstructionsWidget.h"
+
+AInstructionTriggerBox::AInstructionTriggerBox()
+{
+	OnActorBeginOverlap.AddDynamic(this, &AInstructionTriggerBox::OnOverlap);
+}
+
+void AInstructionTriggerBox::OnOverlap(AActor* ThisActor, AActor* OtherActor)
+{
+	UE_LOG(LogTemp, Error, TEXT("OnOverlap !"))
+
+	if(!GetWorld() || !GetWorld()->GetFirstPlayerController())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Fetal Unresonable Error in AInstructionTriggerBox::OnOverlap !"))
+		return;
+	}
+	auto HUD = GetWorld()->GetFirstPlayerController()->GetHUD<AInstructionsHUD>();
+	
+	if(!HUD)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cant Get or Cast HUD in AInstructionTriggerBox::OnOverlap !"))
+		return;
+	}
+	
+	auto InstructionsWidget = HUD->GetInstructionsWidget();
+	if(!InstructionsWidget)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cant Get InstructionsWidget in AInstructionTriggerBox::OnOverlap !"))
+		return;
+	}
+	InstructionsWidget->StartNarrative(Title, &Narrative, &NarrativeTimeInMSecond);
+
+	
+
+	auto LookAtComponent = Cast<ULookAt>(OtherActor->GetComponentByClass(ULookAt::StaticClass()));
+	if(!LookAtComponent)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cant Get LookAtComponent in AInstructionTriggerBox::OnOverlap !"))
+		return;
+	}
+	LookAtComponent->StartLookAt(&LookAtActors, &LookAtTimeInSecond);
+}
+
--- Source/SAVIR_Labs/Public/Components/InstructionTriggerBox.h
+++ Source/SAVIR_Labs/Public/Components/InstructionTriggerBox.h
@@ -13,5 +13,25 @@ UCLASS()
 class SAVIR_LABS_API AInstructionTriggerBox : public ATriggerBox
 {
 	GENERATED_BODY()
+
+
+public:
+	AInstructionTriggerBox();
+
+	UFUNCTION()
+	void OnOverlap(AActor* ThisActor, AActor* OtherActor);
+
+	UPROPERTY(EditInstanceOnly, Category=Data)
+	FString Title;
+	UPROPERTY(EditInstanceOnly, Category=Data)
+	TArray<AActor*>LookAtActors;
+	UPROPERTY(EditInstanceOnly, Category=Data)
+	TArray<float> LookAtTimeInSecond;
+	UPROPERTY(EditInstanceOnly, Category=Data)
+	TArray<FString> Narrative;
+	UPROPERTY(EditInstanceOnly, Category=Data)
+	TArray<float> NarrativeTimeInMSecond;
+	
+	
 	
 };
