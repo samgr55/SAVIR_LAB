b7be371821a23cda45f746f62f08a311dadb2d22
IslamRabie <eslamrbie@gmail.com>
2021-08-15 15:24:30 +0200

    Add IAction Interface

---
+++ Source/SAVIR_Labs/Private/General/Action.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "General/Action.h"
+
+// Add default functionality here for any IAction functions that are not pure virtual.
